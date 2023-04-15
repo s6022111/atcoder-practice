ed6e8d9c989d0a9d7ba67226462f86df20a05b0d
mugicha <s6022111@st.shiga-u.ac.jp>
2023-04-15 19:01:05 +0900

    20230415

--- AtCoder Beginners Selection/testA.py
+++ AtCoder Beginners Selection/testA.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
-# 整数の入力
+# 整数値を入力
 a = int(input())
-# スペース区切りの整数の入力
+
+# スペース区切りで整数値を入力
 b, c = map(int, input().split())
-# 文字列の入力
+
+# 文字列を入力
 s = input()
+
 # 出力
 print("{} {}".format(a+b+c, s))
